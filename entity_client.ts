7e7ae70465d335b8ef1d3e73177d8aef9359e756
Shlomi Vaknin <shlomivaknin@gmail.com>
2019-05-18 01:23:36 -0700

    Add mutable and immutable objectification

--- entity_client.ts
+++ entity_client.ts
@@ -1,26 +1,43 @@
 import axios from "axios"
 import { Metadata, Spec, Status } from 'papiea-core/build/core';
 
-async function create_entity(provider: string, kind: string, version: string, request_spec: Spec, papiea_url: string): Promise<[Metadata, Spec]> {
+interface FullEntity {
+    metadata:Metadata,
+    spec:Spec,
+    status:Status,
+}
+
+interface SpecOnlyEntity {
+    metadata:Metadata,
+    spec:Spec
+}
+
+interface EntityRef {
+    metadata:Metadata
+}
+
+type Entity = EntityRef | SpecOnlyEntity | FullEntity
+
+async function create_entity(provider: string, kind: string, version: string, request_spec: Spec, papiea_url: string): Promise<SpecOnlyEntity> {
     const { data: { metadata, spec } } = await axios.post(`${papiea_url}/${provider}/${version}/${kind}`, {
         spec: request_spec
     });
-    return [metadata, spec];
+    return {metadata, spec};
 }
 
-async function update_entity(provider: string, kind: string, version: string, request_spec: Spec, request_metadata: Metadata, papiea_url: string): Promise<[Metadata, Spec]> {
+async function update_entity(provider: string, kind: string, version: string, request_spec: Spec, request_metadata: Metadata, papiea_url: string): Promise<SpecOnlyEntity> {
     const { data: { metadata, spec } } = await axios.put(`${papiea_url}/${provider}/${version}/${kind}/${request_metadata.uuid}`, {
         spec: request_spec,
         metadata: {
             spec_version: request_metadata.spec_version
         }
     });
-    return [metadata, spec]
+    return {metadata, spec}
 }
 
-async function get_entity(provider: string, kind: string, version: string, request_metadata: Metadata, papiea_url: string): Promise<[Metadata, Spec, Status]> {
+async function get_entity(provider: string, kind: string, version: string, request_metadata: Metadata, papiea_url: string): Promise<FullEntity> {
     const { data: { metadata, spec, status } } = await axios.get(`${papiea_url}/${provider}/${version}/${kind}/${request_metadata.uuid}`);
-    return [metadata, spec, status]
+    return {metadata, spec, status}
 }
 
 async function delete_entity(provider: string, kind: string, version: string, request_metadata: Metadata, papiea_url: string): Promise<void> {
@@ -32,10 +49,11 @@ async function invoker_procedure(provider: string, kind: string, version: string
     return res.data;
 }
 
+// map based crud
 export interface EntityCRUD {
-    get(metadata: Metadata):Promise<[Metadata, Spec, Status]>
-    create(spec: Spec): Promise<[Metadata, Spec]>
-    update(metadata: Metadata, spec: Spec): Promise<[Metadata, Spec]>
+    get(metadata: Metadata):Promise<FullEntity>
+    create(spec: Spec): Promise<SpecOnlyEntity>
+    update(metadata: Metadata, spec: Spec): Promise<SpecOnlyEntity>
     delete(metadata: Metadata): Promise<void>
     invoke_procedure(procedure_name: string, metadata: Metadata, input: any): Promise<any>
 }
@@ -49,4 +67,114 @@ export function kind_crud_api(papiea_url: string, provider: string, kind: string
         invoke_procedure: (proc_name: string, metadata: Metadata, input: any) => invoker_procedure(provider, kind, version, proc_name, input, metadata, papiea_url)
     }
     return crudder
+}
+
+// class based crud
+interface EntityObjectCRUD {
+    update(spec: Spec):Promise<EntityObjectCRUD>
+    delete():Promise<void>
+    refresh():Promise<EntityObjectCRUD>
+    invoke(procedure_name:string, input:any):Promise<any>
+}
+
+export class MutableEntityObject implements EntityObjectCRUD{
+    entity: Entity 
+    crud:EntityCRUD
+
+    constructor(e:Entity, c:EntityCRUD) {
+        this.entity=e
+        this.crud=c
+    }
+    
+    static async create(c:EntityCRUD, spec:Spec):Promise<MutableEntityObject> {
+        const {metadata} = await c.create(spec)
+        return MutableEntityObject.get(c, metadata)
+    }
+
+    static async get(c:EntityCRUD, metadata:Metadata):Promise<MutableEntityObject>{
+        return new MutableEntityObject(await c.get(metadata), c)
+    }
+
+    async refresh():Promise<MutableEntityObject> {
+        this.entity = await this.crud.get((this.entity as EntityRef).metadata)
+        return this
+    }
+
+    async update(spec: any): Promise<MutableEntityObject> {
+        const res = await this.crud.update((this.entity as EntityRef).metadata, spec)
+        return this.refresh()
+    }
+    
+    async delete(): Promise<void> {
+        return this.crud.delete((this.entity as EntityRef).metadata)
+    }
+
+    async invoke(procedure_name: string, input: any): Promise<any> {
+        const ret = this.crud.invoke_procedure(procedure_name, (this.entity as EntityRef).metadata, input)
+        this.refresh()
+        return ret
+    }
+}
+
+interface EntityObjectBuilder {
+    create(spec:Spec):Promise<MutableEntityObject>
+    get(metadata:EntityRef):Promise<MutableEntityObject>
+}
+
+export function objectify(c:EntityCRUD): EntityObjectBuilder{
+    const ret: EntityObjectBuilder = {
+        create: (spec:Spec) => MutableEntityObject.create(c,spec),
+        get: (metadata:EntityRef) => MutableEntityObject.get(c, metadata.metadata)
+    }
+    return ret
+}
+
+export class ImmutableEntityObject implements EntityObjectCRUD{
+    readonly entity: Entity 
+    readonly crud:EntityCRUD
+
+    constructor(e:Entity, c:EntityCRUD) {
+        this.entity=e
+        this.crud=c
+    }
+    
+    static async create(c:EntityCRUD, spec:Spec):Promise<ImmutableEntityObject> {
+        const {metadata} = await c.create(spec)
+        return ImmutableEntityObject.get(c, metadata)
+    }
+
+    static async get(c:EntityCRUD, metadata:Metadata):Promise<ImmutableEntityObject>{
+        return new ImmutableEntityObject(await c.get(metadata), c)
+    }
+
+    async refresh():Promise<ImmutableEntityObject> {
+        return ImmutableEntityObject.get(this.crud, ((this.entity as EntityRef).metadata))   
+    }
+
+    async update(spec: any): Promise<ImmutableEntityObject> {
+        await this.crud.update((this.entity as EntityRef).metadata, spec)
+        return this.refresh()
+    }
+    
+    async delete(): Promise<void> {
+        return this.crud.delete((this.entity as EntityRef).metadata)
+    }
+
+    async invoke(procedure_name: string, input: any): Promise<any> {
+        const ret = this.crud.invoke_procedure(procedure_name, (this.entity as EntityRef).metadata, input)
+        return ret
+    }
+}
+
+interface ImmutableEntityObjectBuilder {
+    create(spec:Spec):Promise<ImmutableEntityObject>
+    get(metadata:EntityRef):Promise<ImmutableEntityObject>
+}
+
+export function objectify_immutable(c:EntityCRUD): ImmutableEntityObjectBuilder{
+    const ret: ImmutableEntityObjectBuilder = {
+        create: (spec:Spec) => ImmutableEntityObject.create(c,spec),
+        get: (metadata:EntityRef) => ImmutableEntityObject.get(c, metadata.metadata)
+    }
+    return ret
 }
--- location_client_example.ts
+++ location_client_example.ts
@@ -1,10 +1,6 @@
 import { Metadata, Spec, Status } from 'papiea-core/build/core'
-import { kind_crud_api } from './entity_client'
+import { kind_crud_api, objectify, objectify_immutable } from './entity_client'
 import axios from 'axios'
-
-function entity_obj(xs: any): any {
-    return { metadata: xs[0], spec: xs[1], status: xs[2] }
-}
 
 async function main() {
 
@@ -14,35 +10,92 @@ async function main() {
     console.log("Create an api for location kind")
     const location_api = kind_crud_api("http://papiea:3000/services", "location_provider", "Location", "0.0.3")
 
-    console.log("\nCreate a new location...")
-    const [metadata] = await location_api.create({ x: 10, y: 20 })
-    console.log("\tCreated location metadata", metadata)
+    {
+        console.log("\nCreate a new location...")
+        const { metadata } = await location_api.create({ x: 10, y: 20 })
+        console.log("\tCreated location metadata", metadata)
 
-    let entity = entity_obj(await location_api.get(metadata))
-    console.log("\tLocation created is:", entity)
+        let entity = await location_api.get(metadata)
+        console.log("\tLocation created is:", entity)
 
-    console.log("\nUpdate location to a new spec")
-    await location_api.update(metadata, { x: 100, y: 200 })
+        console.log("\nUpdate location to a new spec")
+        await location_api.update(metadata, { x: 100, y: 200 })
 
-    entity = entity_obj(await location_api.get(metadata))
-    console.log("\tLocation update to:", entity)
+        entity = await location_api.get(metadata)
+        console.log("\tLocation update to:", entity)
 
-    console.log("\nInvoking moveX procedure...")
-    const res = await location_api.invoke_procedure("moveX", metadata, { input: 100 })
-    console.log("\tProcedure returned:", res);
+        console.log("\nInvoking moveX procedure...")
+        const res = await location_api.invoke_procedure("moveX", metadata, { input: 100 })
+        console.log("\tProcedure returned:", res);
 
-    entity = entity_obj(await location_api.get(metadata))
-    console.log("\tProcedure transformed to:", entity)
+        entity = await location_api.get(metadata)
+        console.log("\tProcedure transformed to:", entity)
 
-    console.log("\nDeleteing the entity...")
-    //Delete entity on provider with kind
-    await location_api.delete(metadata);
+        console.log("\nDeleteing the entity...")
+        //Delete entity on provider with kind
+        await location_api.delete(metadata);
 
-    try {
-        entity = entity_obj(await location_api.get(metadata))
-        console.log("\tShould not see this location!", entity)
-    } catch (e) {
-        console.log("\tLocation not found (good, it was deleted!)", e.message)
+        try {
+            entity = await location_api.get(metadata)
+            console.log("\tShould not see this location!", entity)
+        } catch (e) {
+            console.log("\tLocation not found (good, it was deleted!)", e.message)
+        }
+    }
+
+    // The same exact test as above but with object representation
+    {
+        const location_oapi = objectify(location_api)
+        console.log("\nCreate a new location...")
+        let loc = await location_oapi.create({ x: 10, y: 20 })
+        console.log("\tLocation created is:", loc.entity)
+
+        console.log("\nUpdate location to a new spec")
+        await loc.update({ x: 100, y: 200 })
+        console.log("\tLocation update to:", loc.entity)
+
+        console.log("\nInvoking moveX procedure...")
+        const proc_res = await loc.invoke("moveX", { input: 100 })
+        console.log("\tProcedure returned:", proc_res);
+        console.log("\tProcedure transformed to:", loc.entity)
+
+        console.log("\nDeleteing the entity...")
+        await loc.delete()
+
+        try {
+            const entity = await location_oapi.get(loc.entity)
+            console.log("\tShould not see this location!", entity)
+        } catch (e) {
+            console.log("\tLocation not found (good, it was deleted!)", e.message)
+        }
+    }
+    {
+        // The same exact test as above but with immutable object representation
+        const location_imoapi = objectify_immutable(location_api)
+        console.log("\nCreate a new location...")
+        const immloc = await location_imoapi.create({ x: 10, y: 20 })
+        console.log("\tLocation created is:", immloc.entity)
+
+        console.log("\nUpdate location to a new spec")
+        await immloc.update({ x: 100, y: 200 })
+        console.log("\tLocation update to:", immloc.entity)
+
+        console.log("\nInvoking moveX procedure...")
+        const proc_res2 = await immloc.invoke("moveX", { input: 100 })
+        console.log("\tProcedure returned:", proc_res2);
+        console.log("\tProcedure original to:", immloc.entity)
+        const immloc_updated = await location_imoapi.get(immloc.entity)
+        console.log("\tProcedure transformed to:", immloc_updated.entity)
+
+        console.log("\nDeleteing the entity...")
+        await immloc_updated.delete()
+
+        try {
+            const entity = await location_imoapi.get(immloc_updated.entity)
+            console.log("\tShould not see this location!", entity)
+        } catch (e) {
+            console.log("\tLocation not found (good, it was deleted!)", e.message)
+        }
     }
 }
 
