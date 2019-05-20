361d7ec031e378c08578c3c733dacaa44b1aceb3
Shlomi Vaknin <shlomivaknin@gmail.com>
2019-05-19 18:30:16 -0700

    Improve getter semantics and names. Rename package.

--- entity_client.ts
+++ entity_client.ts
@@ -2,14 +2,14 @@ import axios from "axios"
 import { Metadata, Spec, Status, Entity_Reference } from 'papiea-core/build/core';
 
 interface FullEntity {
-    metadata:Metadata,
-    spec:Spec,
-    status:Status,
+    metadata: Metadata,
+    spec: Spec,
+    status: Status,
 }
 
 interface EntitySpec {
-    metadata:Metadata,
-    spec:Spec
+    metadata: Metadata,
+    spec: Spec
 }
 
 type Entity = EntitySpec | FullEntity
@@ -18,7 +18,7 @@ async function create_entity(provider: string, kind: string, version: string, re
     const { data: { metadata, spec } } = await axios.post(`${papiea_url}/${provider}/${version}/${kind}`, {
         spec: request_spec
     });
-    return {metadata, spec};
+    return { metadata, spec };
 }
 
 async function update_entity(provider: string, kind: string, version: string, request_spec: Spec, request_metadata: Metadata, papiea_url: string): Promise<EntitySpec> {
@@ -28,67 +28,68 @@ async function update_entity(provider: string, kind: string, version: string, re
             spec_version: request_metadata.spec_version
         }
     });
-    return {metadata, spec}
+    return { metadata, spec }
 }
 
-async function get_entity(provider: string, kind: string, version: string, request_metadata: Entity_Reference, papiea_url: string): Promise<FullEntity> {
-    const { data: { metadata, spec, status } } = await axios.get(`${papiea_url}/${provider}/${version}/${kind}/${request_metadata.uuid}`);
-    return {metadata, spec, status}
+async function get_entity(provider: string, kind: string, version: string, entity_reference: Entity_Reference, papiea_url: string): Promise<FullEntity> {
+    const { data: { metadata, spec, status } } = await axios.get(`${papiea_url}/${provider}/${version}/${kind}/${entity_reference.uuid}`);
+    return { metadata, spec, status }
 }
 
-async function delete_entity(provider: string, kind: string, version: string, request_metadata: Entity_Reference, papiea_url: string): Promise<void> {
-    await axios.delete(`${papiea_url}/${provider}/${version}/${kind}/${request_metadata.uuid}`);
+async function delete_entity(provider: string, kind: string, version: string, entity_reference: Entity_Reference, papiea_url: string): Promise<void> {
+    await axios.delete(`${papiea_url}/${provider}/${version}/${kind}/${entity_reference.uuid}`);
 }
 
-async function invoker_procedure(provider: string, kind: string, version: string, procedure_name: string, input: any, request_metadata: Metadata, papiea_url: string): Promise<any> {
-    const res = await axios.post(`${papiea_url}/${provider}/${version}/${kind}/${request_metadata.uuid}/procedure/${procedure_name}`, input);
+async function invoker_procedure(provider: string, kind: string, version: string, procedure_name: string, input: any, entity_reference: Entity_Reference, papiea_url: string): Promise<any> {
+    const res = await axios.post(`${papiea_url}/${provider}/${version}/${kind}/${entity_reference.uuid}/procedure/${procedure_name}`, input);
     return res.data;
 }
 
 // map based crud
 export interface EntityCRUD {
-    get(metadata: Entity_Reference):Promise<FullEntity>
+    get(entity_reference: Entity_Reference): Promise<FullEntity>
     create(spec: Spec): Promise<EntitySpec>
     update(metadata: Metadata, spec: Spec): Promise<EntitySpec>
-    delete(metadata: Metadata): Promise<void>
-    invoke_procedure(procedure_name: string, metadata: Metadata, input: any): Promise<any>
+    delete(entity_reference: Entity_Reference): Promise<void>
+    invoke_procedure(procedure_name: string, entity_reference: Entity_Reference, input: any): Promise<any>
 }
 
 export function kind_client(papiea_url: string, provider: string, kind: string, version: string): EntityCRUD {
     const crudder: EntityCRUD = {
-        get: (metadata: Metadata) => get_entity(provider, kind, version, metadata, papiea_url),
+        get: (entity_reference: Entity_Reference) => get_entity(provider, kind, version, entity_reference, papiea_url),
         create: (spec: Spec) => create_entity(provider, kind, version, spec, papiea_url),
         update: (metadata: Metadata, spec: Spec) => update_entity(provider, kind, version, spec, metadata, papiea_url),
-        delete: (metadata: Metadata) => delete_entity(provider, kind, version, metadata, papiea_url),
-        invoke_procedure: (proc_name: string, metadata: Metadata, input: any) => invoker_procedure(provider, kind, version, proc_name, input, metadata, papiea_url)
+        delete: (entity_reference: Entity_Reference) => delete_entity(provider, kind, version, entity_reference, papiea_url),
+        invoke_procedure: (proc_name: string, entity_reference: Entity_Reference, input: any) => invoker_procedure(provider, kind, version, proc_name, input, entity_reference, papiea_url)
     }
     return crudder
 }
 
 // class based crud
 interface EntityObjectCRUD {
-    update(spec: Spec):Promise<EntityObjectCRUD>
-    delete():Promise<void>
-    invoke(procedure_name:string, input:any):Promise<any>
+    update(spec: Spec): Promise<EntityObjectCRUD>
+    delete(): Promise<void>
+    invoke(procedure_name: string, input: any): Promise<any>
 }
 
-export class ImmutableEntityObject implements EntityObjectCRUD{
-    readonly entity: Entity 
-    readonly crud:EntityCRUD
+export class ImmutableEntityObject implements EntityObjectCRUD {
+    readonly entity: Entity
+    readonly crud: EntityCRUD
 
-    constructor(e:Entity, c:EntityCRUD) {
-        this.entity=e
-        this.crud=c
+    constructor(e: Entity, c: EntityCRUD) {
+        this.entity = e
+        this.crud = c
     }
 
-    async refresh():Promise<ImmutableEntityObject> {
+    async refresh(): Promise<ImmutableEntityObject> {
         return new ImmutableEntityObject(await this.crud.get(this.entity.metadata), this.crud)
     }
 
     async update(spec: any): Promise<ImmutableEntityObject> {
-        return this.crud.update(this.entity.metadata, spec).then(_=>this.refresh())
+        const _ = await this.crud.update(this.entity.metadata, spec);
+        return await this.refresh();
     }
-    
+
     delete(): Promise<void> {
         return this.crud.delete(this.entity.metadata)
     }
@@ -99,13 +100,13 @@ export class ImmutableEntityObject implements EntityObjectCRUD{
 }
 
 interface ImmutableEntityObjectBuilder {
-    create(spec:Spec):Promise<ImmutableEntityObject>
-    get(metadata:Entity_Reference):Promise<ImmutableEntityObject>
+    create(spec: Spec): Promise<ImmutableEntityObject>
+    get(entity_reference: Entity_Reference): Promise<ImmutableEntityObject>
 }
 
-export function objectify(c:EntityCRUD): ImmutableEntityObjectBuilder{
-   return {
-        create: async (spec:Spec) => new ImmutableEntityObject(await c.create(spec), c).refresh(),
-        get: async (metadata:Entity_Reference) => new ImmutableEntityObject(await c.get(metadata), c)
+export function objectify(c: EntityCRUD): ImmutableEntityObjectBuilder {
+    return {
+        create: async (spec: Spec) => new ImmutableEntityObject(await c.create(spec), c).refresh(),
+        get: async (entity_reference: Entity_Reference) => new ImmutableEntityObject(await c.get(entity_reference), c)
     }
 }
